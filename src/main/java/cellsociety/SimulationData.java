451922a9319bd74460a4babe8dfafb0dba5ffe68
Justin Aronwald <jga18@duke.edu>
2025-01-29 13:59:51 -0500

    Feature DESIGN-08: added javadoc comments to the SimulationData class

--- src/main/java/cellsociety/SimulationData.java
+++ src/main/java/cellsociety/SimulationData.java
@@ -18,42 +18,82 @@ public class SimulationData {
     myColors = colors;
   }
 
+  /**
+   *
+   * @return the type of the simulation
+   */
   public String getType() {
     return myType;
   }
 
+  /**
+   *
+   * @return the name of the simulation
+   */
   public String getName() {
     return myName;
   }
 
+  /**
+   *
+   * @return the author of the simulation
+   */
   public String getAuthor() {
     return myAuthor;
   }
 
+  /**
+   *
+   * @return the description of the simulation
+   */
   public String getDescription() {
     return myDescription;
   }
 
+  /**
+   *
+   * @return the colors of the simulation
+   */
   public ArrayList<Color> getColors() {
     return myColors;
   }
 
+  /**
+   *
+   * @param type - the new type of simulation
+   */
   public void setType(String type) {
     myType = type;
   }
 
+  /**
+   *
+   * @param name - the new name of the simulation
+   */
   public void setName(String name) {
     myName = name;
   }
 
+  /**
+   *
+   * @param author - the new author of the simulation
+   */
   public void setAuthor(String author) {
     myAuthor = author;
   }
 
+  /**
+   *
+   * @param description - the new description of the simulation
+   */
   public void setDescription(String description) {
     myDescription = description;
   }
 
+  /**
+   *
+   * @param colors - the new list of colors of the simulation
+   */
   public void setColors(ArrayList<Color> colors) {
     myColors = colors;
   }
