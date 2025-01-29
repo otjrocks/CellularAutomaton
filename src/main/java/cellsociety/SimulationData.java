238900d6f08d770a2f1a29266fafa193d0ecdc98
Justin Aronwald <jga18@duke.edu>
2025-01-29 13:56:44 -0500

    Feature CELL-01: first draft of SimulationData wiht getters/setters

--- src/main/java/cellsociety/SimulationData.java
+++ src/main/java/cellsociety/SimulationData.java
@@ -1,5 +1,62 @@
 package cellsociety;
 
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+
 public class SimulationData {
+  private String myType;
+  private String myName;
+  private String myAuthor;
+  private String myDescription;
+  private ArrayList<Color> myColors;
+
+  public SimulationData(String type, String name, String author, String description, ArrayList<Color> colors) {
+    myType = type;
+    myName = name;
+    myAuthor = author;
+    myDescription = description;
+    myColors = colors;
+  }
+
+  public String getType() {
+    return myType;
+  }
+
+  public String getName() {
+    return myName;
+  }
+
+  public String getAuthor() {
+    return myAuthor;
+  }
+
+  public String getDescription() {
+    return myDescription;
+  }
+
+  public ArrayList<Color> getColors() {
+    return myColors;
+  }
+
+  public void setType(String type) {
+    myType = type;
+  }
+
+  public void setName(String name) {
+    myName = name;
+  }
+
+  public void setAuthor(String author) {
+    myAuthor = author;
+  }
+
+  public void setDescription(String description) {
+    myDescription = description;
+  }
+
+  public void setColors(ArrayList<Color> colors) {
+    myColors = colors;
+  }
+
 
 }
