a916f91f0e08d5a05ea95fe102ba63cbabe07366
Justin Aronwald <jga18@duke.edu>
2025-01-31 15:04:10 -0500

    Test CELL-03: Added test file to ensure SpreadingOfFires ran properly. It seems like it did

---
+++ src/main/java/cellsociety/SpreadingOfFireExample.java
@@ -0,0 +1,49 @@
+package cellsociety;
+
+import cellsociety.model.Grid;
+import cellsociety.model.cell.DefaultCell;
+import cellsociety.model.simulation.SimulationData;
+import cellsociety.model.simulation.rules.PercolationRules;
+import cellsociety.model.simulation.rules.SpreadingOfFireRules;
+import cellsociety.model.simulation.types.Percolation;
+import cellsociety.model.simulation.types.SpreadingOfFire;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SpreadingOfFireExample {
+
+  public static void main(String[] args) throws InterruptedException {
+    int width = 10, height = 10;
+    Grid grid = new Grid(width, height);
+
+    //randomness aspect generated from ChatGPT
+    Random random = new Random();
+    double openProbability = 0.7; // 70% chance to be open, 30% chance to be blocked
+
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        int state = (random.nextDouble() < openProbability) ? 1 : 0;
+        grid.addCell(new DefaultCell(state, new Point2D.Double(x, y)));
+      }
+    }
+
+    grid.addCell(new DefaultCell(2, new Point2D.Double(4, 3)));
+    grid.addCell(new DefaultCell(2, new Point2D.Double(5, 3)));
+    grid.addCell(new DefaultCell(2, new Point2D.Double(4, 6)));
+    grid.addCell(new DefaultCell(2, new Point2D.Double(5, 6)));
+
+    SpreadingOfFire sof = new SpreadingOfFire(
+        new SpreadingOfFireRules(),
+        new SimulationData("SOF", "SOF", "Author", "SOF Simulation", new ArrayList<>())
+    );
+
+    for (int i = 0; i < 40; i++) {
+      System.out.println("Step: " + i);
+      grid.printGrid();
+      Thread.sleep(500);
+      grid.updateGrid(sof);
+      System.out.println("\n\n\n\n");
+    }
+  }
+}
