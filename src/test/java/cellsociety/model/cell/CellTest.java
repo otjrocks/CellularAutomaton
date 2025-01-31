761a8234247f8b80d89200daba26f90f477efaff
Owen Jennings <otj4@duke.edu>
2025-01-31 11:52:40 -0500

    FILE: added JUnit directory and first test file

---
+++ src/test/java/cellsociety/model/cell/CellTest.java
@@ -0,0 +1,34 @@
+package cellsociety.model.cell;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CellTest {
+
+  @BeforeEach
+  void setUp() {
+
+  }
+
+  @Test
+  void getState() {
+  }
+
+  @Test
+  void setState() {
+  }
+
+  @Test
+  void getLocation() {
+  }
+
+  @Test
+  void getRow() {
+  }
+
+  @Test
+  void getCol() {
+  }
+}
