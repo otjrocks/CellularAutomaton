07972fe1f85b2c592a1a20bf0489568ff477868a
Justin Aronwald <jga18@duke.edu>
2025-01-31 15:25:31 -0500

    Feature CELL_04: Schellings model of segregation simulation class first draft complete

---
+++ src/main/java/cellsociety/model/simulation/types/SegregationModel.java
@@ -0,0 +1,32 @@
+package cellsociety.model.simulation.types;
+
+import cellsociety.model.simulation.Simulation;
+import cellsociety.model.simulation.SimulationData;
+import cellsociety.model.simulation.SimulationRules;
+import cellsociety.model.simulation.StateInfo;
+import javafx.scene.paint.Color;
+
+public class SegregationModel extends Simulation {
+
+  public SegregationModel(SimulationRules rules, SimulationData data) {
+    super(rules, data);
+  }
+
+  /**
+   *
+   */
+  @Override
+  public void getNextSimulationState() {
+    return;
+  }
+
+  /**
+   *
+   */
+  @Override
+  protected void initializeStateMap() {
+    stateMap.put(0, new StateInfo("Empty", Color.WHITE));
+    stateMap.put(1, new StateInfo("Group1", Color.RED));
+    stateMap.put(2, new StateInfo("Group2", Color.BLUE));
+  }
+}
