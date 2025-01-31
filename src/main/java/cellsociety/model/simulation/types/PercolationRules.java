63563265ba93abcf78b70ba945d9b8827bc3108b
Justin Aronwald <jga18@duke.edu>
2025-01-31 01:16:00 -0500

    Feature CELL-02: percolationRules first draft completed (assumes only using 4 neighbors, water flows from top, and can flow in all directions)

---
+++ src/main/java/cellsociety/model/simulation/types/PercolationRules.java
@@ -0,0 +1,83 @@
+package cellsociety.model.simulation.types;
+
+import cellsociety.model.Grid;
+import cellsociety.model.cell.Cell;
+import cellsociety.model.simulation.SimulationRules;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+//For a Percolation cell, there can be 3 states
+// A cell with state 0 indicates it's blocked
+// A cell with state 1 indicates it's open and water can pass through
+// A cell with state 2 indicates it's filled and water has passed through this cell
+
+public class PercolationRules extends SimulationRules {
+  private Map<String, Double> parameters;
+
+  public PercolationRules(Map<String, Double> parameters) {
+    super(parameters);
+  }
+
+  /**
+   * @param cell  - an individual cell from the grid
+   * @param grid - the list of cell objects representing the grid
+   * @return -  a list of cell objects representing the neighbors of the cell (adjacent and
+   *    * diagonals)
+   */
+  @Override
+  public List<Cell> getNeighbors(Cell cell, Grid grid) {
+    List<Cell> neighbors = new ArrayList<>();
+
+    int [][] directions = {
+        {-1, 0}, {1, 0}, {0, -1}, {0, 1} // left, up, down, right
+    };
+
+    for (int[] direction : directions) {
+      Point2D neighborLocation = new Point2D.Double(cell.getRow() + direction[0], cell.getCol() + direction[1]);
+
+      if (grid.cellExists(neighborLocation)){
+        Cell neighbor = grid.getCell(neighborLocation);
+        if (neighbor.getState() == 1){
+          neighbors.add(neighbor);
+        }
+      }
+    }
+    return neighbors;
+  }
+
+  /**
+   * Percolation:
+   * Water starts form the top row of the grid.
+   * Any open cell in the first row becomes filled
+   * If a cell below a filled cell is open, it becomes filled
+   * If a neighboring cell (not including diagonals) is open and is connected to another filled cell, it becomes filled
+   * If a bottom row cell becomes filled, percolation is successful
+   *
+   * @param cell - an individual cell from the grid
+   * @param grid - the list of cell objects representing the grid
+   * @return - the next state of a cell based on the rules of percolation
+   */
+  @Override
+  public int getNextState(Cell cell, Grid grid) {
+    if (cell.getState() == 1) {
+      return 1;
+    }
+    if (cell.getState() == 2) {
+      return 2;
+    }
+    if (cell.getRow() == 0 && cell.getState() == 1) {
+      return 2;
+    }
+
+    List<Cell> neighbors = getNeighbors(cell, grid);
+    for (Cell neighbor : neighbors) {
+      if (neighbor.getState() == 2) {
+        return 2;
+      }
+    }
+
+    return 1;
+  }
+}
