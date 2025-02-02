ec7a04a3a1e6ab4ff194dc18ba18f0bb7ccc8f0e
Justin Aronwald <jga18@duke.edu>
2025-02-02 14:20:05 -0500

    Test CELL-01: Experimenting with tests, started for grid SimulationRules

---
+++ src/test/java/cellsociety/model/simulation/SimulationRulesTest.java
@@ -0,0 +1,88 @@
+package cellsociety.model.simulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import cellsociety.model.Grid;
+import cellsociety.model.cell.Cell;
+import cellsociety.model.cell.DefaultCell;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.awt.geom.Point2D.Double;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SimulationRulesTest {
+  private Grid grid;
+  private SimulationRules testSimulationRules;
+
+  @BeforeEach
+  void setUp() {
+    grid = new Grid(5, 5);
+    testSimulationRules = new SimulationRules() {
+      @Override
+      public List<Cell> getNeighbors(Cell cell, Grid grid) {
+        return new ArrayList<>();
+      }
+
+      @Override
+      public int getNextState(Cell cell, Grid grid) {
+        return cell.getState();
+      }
+    };
+  }
+  @Test
+  void testDefaultConstructor() {
+    assertNull(testSimulationRules.getParameter("noKey"),"Default constructor creates an empty parameter map.");
+  }
+/*
+  @Test
+  void testParameterConstructor() {
+    Map<String, Double> params = new HashMap<>();
+    params.put("fire", 0.7);
+    params.put("water", 0.3);
+    testSimulationRules = new SimulationRules(params) {
+      @Override
+      public List<Cell> getNeighbors(Cell cell, Grid grid) {
+        return List.of();
+      }
+
+      @Override
+      public int getNextState(Cell cell, Grid grid) {
+        return 0;
+      }
+    };
+
+    assertEquals(0.7, testSimulationRules.getParameter("fire"),"Parameter constructor creates a parameter map.");
+    assertEquals(0.3, testSimulationRules.getParameter("water"),"Parameter constructor creates a parameter map.");
+  }
+*/
+  @Test
+  void testSetAndGetParameter() {
+    testSimulationRules.setParameter("fire", 0.7);
+    assertEquals(0.7, testSimulationRules.getParameter("fire"));
+  }
+
+  @Test
+  void testNoParameter() {
+    assertNull(testSimulationRules.getParameter("noParameter"));
+  }
+
+
+  @Test
+  void getNeighbors() {
+    Grid grid = new Grid(5, 5);
+    Cell cell = new Cell(1, new Double(1, 1)) {
+    };
+
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertTrue(neighbors.isEmpty(), "Neighbor list is empty.");
+  }
+
+  @Test
+  void getNextState() {
+  }
+}
---
+++ src/test/java/cellsociety/model/simulation/SimulationTest.java
@@ -0,0 +1,28 @@
+package cellsociety.model.simulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SimulationTest {
+
+  @Test
+  void getRules() {
+  }
+
+  @Test
+  void getData() {
+  }
+
+  @Test
+  void getNextSimulationState() {
+  }
+
+  @Test
+  void initializeStateMap() {
+  }
+
+  @Test
+  void getStateInfo() {
+  }
+}
