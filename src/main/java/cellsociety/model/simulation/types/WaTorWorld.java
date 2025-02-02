ee8332fb15c792c3d56e59d1f770fc80d7fd9d67
Justin Aronwald <jga18@duke.edu>
2025-02-02 15:11:18 -0500

    Feature CELL-05: first draft of the wa-torworld class completed

---
+++ src/main/java/cellsociety/model/simulation/types/WaTorWorld.java
@@ -0,0 +1,23 @@
+package cellsociety.model.simulation.types;
+
+import cellsociety.model.simulation.Simulation;
+import cellsociety.model.simulation.SimulationData;
+import cellsociety.model.simulation.SimulationRules;
+import cellsociety.model.simulation.StateInfo;
+import javafx.scene.paint.Color;
+
+public class WaTorWorld extends Simulation {
+
+
+  public WaTorWorld(SimulationRules rules,
+      SimulationData data) {
+    super(rules, data);
+  }
+
+  @Override
+  protected void initializeStateMap() {
+    stateMap.put(0, new StateInfo("Empty", Color.BLACK));
+    stateMap.put(1, new StateInfo("Fish", Color.GREEN));
+    stateMap.put(2, new StateInfo("Sharks", Color.BLUE));
+  }
+}
