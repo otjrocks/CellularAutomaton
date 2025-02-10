7e5c1835a8736e84b4196b39fdfbcc9f9d3decc2
Justin Aronwald <jga18@duke.edu>
2025-02-10 01:05:57 -0500

    Feature CEll-36: added funcitonality to parse the properites folder and retrieve the languages. Now adds them to a dropdown in the sidebar

--- src/main/java/cellsociety/view/SidebarView.java
+++ src/main/java/cellsociety/view/SidebarView.java
@@ -4,10 +4,15 @@ import static cellsociety.config.MainConfig.MESSAGES;
 
 import cellsociety.controller.MainController;
 import cellsociety.view.components.AlertField;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * A class to handle all the sidebar view elements of the app
@@ -21,9 +26,12 @@ public class SidebarView extends VBox {
   private final MainController myMainController;
   private boolean isEditing = false;
   private Button myModeButton;
+  private ComboBox<String> languageDropdown;
+  private Text changeLanguageText;
   private AlertField myAlertField;
   private final EditModeView myEditModeView;
   private final ViewModeView myViewModeView;
+
 
   /**
    * Create a sidebar view with a preferred size of width x height
@@ -38,9 +46,10 @@ public class SidebarView extends VBox {
     myMainController = controller;
     initializeAlertField();
     createChangeModeButton();
+    createLanguageDropdown();
     myViewModeView = new ViewModeView(myMainController, myAlertField);
     myEditModeView = new EditModeView(myMainController, myAlertField);
-    this.getChildren().addAll(myModeButton, myViewModeView, myAlertField);
+    this.getChildren().addAll(myModeButton, myViewModeView, myAlertField, changeLanguageText, languageDropdown);
   }
 
   public void update() {
@@ -57,14 +66,14 @@ public class SidebarView extends VBox {
   private void disableEditView() {
     this.getChildren().clear();
     myViewModeView.update();
-    this.getChildren().addAll(myModeButton, myViewModeView, myAlertField);
+    this.getChildren().addAll(myModeButton, myViewModeView, myAlertField, changeLanguageText, languageDropdown);
   }
 
 
   private void enableEditView() {
     myEditModeView.updateStateInfo();
     this.getChildren().clear();
-    this.getChildren().addAll(myModeButton, myEditModeView, myAlertField);
+    this.getChildren().addAll(myModeButton, myEditModeView, myAlertField, changeLanguageText, languageDropdown);
   }
 
   private void createChangeModeButton() {
@@ -85,4 +94,47 @@ public class SidebarView extends VBox {
       }
     });
   }
+
+  private void createLanguageDropdown() {
+    languageDropdown = new ComboBox<>();
+    changeLanguageText = new Text(MESSAGES.getString("CHANGE_LANGUAGE"));
+
+    String propertiesFolderPath = "src/main/resources/cellsociety/languages/";
+    List<String> languages = fetchLanguages(propertiesFolderPath);
+
+    if (languages.isEmpty()) {
+      myAlertField.flash(MESSAGES.getString("NO_LANGUAGES_FOUND"), false);
+    }
+
+    for (String language : languages) {
+      languageDropdown.getItems().add(language);
+    }
+    if (languages.contains("English")) {
+      languageDropdown.setValue("English");
+    } else {
+      languageDropdown.setValue(languages.getFirst());
+    }
+
+    languageDropdown.setOnAction(event -> {
+      String language = languageDropdown.getValue();
+      System.out.println("Selected language: " + language);
+    });
+  }
+
+  private List<String> fetchLanguages(String propertiesFolderPath) {
+    List<String> languages = new ArrayList<>();
+    File directory = new File(propertiesFolderPath);
+    // this next line was from an LLM
+    File[] files = directory.listFiles((dir, name) -> name.endsWith(".properties"));
+
+    if (files != null) {
+      for (File file : files) {
+        String fileNameWithoutExtension = file.getName().replace(".properties", "");
+        languages.add(fileNameWithoutExtension);
+      }
+    }
+
+    return languages;
+  }
+
 }
