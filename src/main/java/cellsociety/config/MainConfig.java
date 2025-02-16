c01ac46724089dac908e66c265e5ba8b3064afc0
Owen Jennings <otj4@duke.edu>
2025-02-16 15:52:35 -0500

    Test CELL-33: added test to ensure preferences state is being stored and retrieved correctly - closes #81

--- src/main/java/cellsociety/config/MainConfig.java
+++ src/main/java/cellsociety/config/MainConfig.java
@@ -30,10 +30,10 @@ public class MainConfig {
   }
 
   public static final String TITLE = getMessages().getString("TITLE");
-  public static final int WIDTH = 1400;
+  public static final int WIDTH = 1500;
   public static final int HEIGHT = 800;
   public static final int MARGIN = 20;
-  public static final int GRID_WIDTH = (2 * WIDTH) / 3;
+  public static final int GRID_WIDTH = (3 * WIDTH) / 5;
   public static final int GRID_HEIGHT = HEIGHT - (2 * MARGIN);
   public static final double INITIAL_STEP_SPEED = 0.5;
   public static final double STEP_SPEED =
