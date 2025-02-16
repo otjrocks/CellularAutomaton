d9e8367e8f1d6bc86ad65d4d2c1f153924295a94
Owen Jennings <otj4@duke.edu>
2025-02-15 23:42:19 -0500

    Feature CELL-26B: added first version of falling sand rules

--- src/main/java/cellsociety/config/SimulationConfig.java
+++ src/main/java/cellsociety/config/SimulationConfig.java
@@ -32,7 +32,8 @@ public class SimulationConfig {
       "Segregation",
       "SpreadingOfFire",
       "WaTorWorld",
-      "RockPaperScissors"
+      "RockPaperScissors",
+      "FallingSand"
   };
 
   /**
