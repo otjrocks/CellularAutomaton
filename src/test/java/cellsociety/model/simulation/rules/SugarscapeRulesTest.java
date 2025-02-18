e4be45d30576169a75408b6dca5237fdd214eb2f
Justin Aronwald <jga18@duke.edu>
2025-02-18 00:40:32 -0500

    Test CELL-26F: added more tests for sugarscape

--- src/test/java/cellsociety/model/simulation/rules/SugarscapeRulesTest.java
+++ src/test/java/cellsociety/model/simulation/rules/SugarscapeRulesTest.java
@@ -10,6 +10,7 @@ import cellsociety.model.cell.DefaultCell;
 import cellsociety.model.cell.PatchCell;
 import cellsociety.model.simulation.InvalidParameterException;
 import cellsociety.model.simulation.Parameter;
+import cellsociety.model.simulation.rules.SugarscapeRules.State;
 import java.awt.geom.Point2D;
 import java.util.HashMap;
 import java.util.List;
@@ -56,7 +57,7 @@ class SugarscapeRulesTest {
   }
 
   @Test
-  void getNextStatesForAllCells_AgentMovesToRichestPatch() {
+  void getNextStatesForAllCells_AgentMovesToRichestPatch_UpdatedAgentCellAndSugar() {
     AgentCell agent = new AgentCell(SugarscapeRules.State.AGENTS.getValue(), new Point2D.Double(2, 2), 2, 1, 10);
     PatchCell lowSugarPatch = new PatchCell(SugarscapeRules.State.PATCHES.getValue(), 2, 3, 1, new Point2D.Double(2, 3));
     PatchCell anotherLowSugarPatch = new PatchCell(SugarscapeRules.State.PATCHES.getValue(), 2, 3, 1, new Point2D.Double(2, 4));
@@ -69,18 +70,66 @@ class SugarscapeRulesTest {
 
     List<CellUpdate> updates = sugarscapeRules.getNextStatesForAllCells(grid);
     boolean consumedPatch = false;
+    int newSugar = -1;
+
     for (CellUpdate update : updates) {
-      if (update.getNextCell().getLocation().equals(new Point2D.Double(3, 2))) {
+      if (update.getNextCell().getLocation().equals(new Point2D.Double(3, 2)) && update.getNextCell().getState() == State.AGENTS.getValue()) {
         consumedPatch = true;
-        break;
+        AgentCell agentCell = (AgentCell) update.getNextCell();
+        newSugar = agentCell.getSugar();
+      }
+
+      if (update.getNextCell().getLocation().equals(new Point2D.Double(3, 2)) && update.getNextCell().getState() == State.PATCHES.getValue()) {
+        PatchCell patchCell = (PatchCell) update.getNextCell();
+        assertEquals(0, patchCell.getSugar(), "Path cell reset to 0");
       }
     }
 
     assertTrue(consumedPatch, "Agent cell moved to the biggest patch");
-    assertEquals(12, updates);
-    assertEquals(3, anotherLowSugarPatch.getSugar());
+    assertEquals(17, newSugar, "Agent (10) consumed biggest patch (7) but lost 1 due to metabolism");
   }
 
+  @Test
+  void getNextStatesForAllCells_AgentChoosesMinDistanceInTie_UpdatedAgentCell() {
+    AgentCell agent = new AgentCell(SugarscapeRules.State.AGENTS.getValue(), new Point2D.Double(2, 2), 3, 1, 10);
+    PatchCell patch1 = new PatchCell(SugarscapeRules.State.PATCHES.getValue(), 5, 3, 1, new Point2D.Double(2, 3));
+    PatchCell patch2 = new PatchCell(SugarscapeRules.State.PATCHES.getValue(), 5, 3, 1, new Point2D.Double(2, 4));
+    PatchCell patch3 = new PatchCell(SugarscapeRules.State.PATCHES.getValue(), 5, 3, 1, new Point2D.Double(4, 2));
+
+    grid.updateCell(agent);
+    grid.updateCell(patch1);
+    grid.updateCell(patch2);
+    grid.updateCell(patch3);
+
+    List<CellUpdate> updates = sugarscapeRules.getNextStatesForAllCells(grid);
+    boolean consumedPatch = false;
+
+    for (CellUpdate update : updates) {
+      if (update.getNextCell().getLocation().equals(new Point2D.Double(2, 3)) && update.getNextCell().getState() == State.AGENTS.getValue()) {
+        consumedPatch = true;
+        break;
+      }
+    }
+    assertTrue(consumedPatch, "Agent cell moved to the closest patch at (2,3) ");
+  }
+
+  @Test
+  void getNextStatesForAllCells_SugarReachesZero_AgentDies() {
+    AgentCell agent = new AgentCell(SugarscapeRules.State.AGENTS.getValue(), new Point2D.Double(2, 2), 2, 10, 5);
+
+    grid.updateCell(agent);
+
+    List<CellUpdate> updates = sugarscapeRules.getNextStatesForAllCells(grid);
+    boolean agentDied = false;
+
+    for (CellUpdate update : updates) {
+      if (update.getNextCell().getState() == State.EMPTY.getValue()) {
+        agentDied = true;
+        break;
+      }
+    }
+    assertTrue(agentDied, "Agent cell did not move and died");
+  }
 
 
 
