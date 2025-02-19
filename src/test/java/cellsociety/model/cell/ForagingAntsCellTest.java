72ebb313e83d64a661b9ae8ecb3a3b30a786cb79
troy.ludwig <tdl21@duke.edu>
2025-02-18 19:57:19 -0500

    Test: Added ForagingAntCellTest #70

--- src/test/java/cellsociety/model/cell/ForagingAntsCellTest.java
+++ src/test/java/cellsociety/model/cell/ForagingAntsCellTest.java
@@ -2,18 +2,19 @@ package cellsociety.model.cell;
 
 import java.awt.geom.Point2D.Double;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ForagingAntsCellTest {
 
-  Cell cellOne;
-  Cell cellTwo;
+  ForagingAntsCell cellOne;
+  ForagingAntsCell cellTwo;
 
   @BeforeEach
   void setUp() {
-    cellOne = new ForagingAntsCell(5, new Double(0, 1)) {
+    cellOne = new ForagingAntsCell(5, new Double(0, 1), 10.0, 25.0, 500, true) {
     };
   }
 
@@ -34,6 +35,32 @@ class ForagingAntsCellTest {
     assertThrows(IllegalArgumentException.class, () ->
         cellTwo = new ForagingAntsCell(0, new Double(5.5, 0) ,1.0, 1.0, 10, false) {
         });
+  }
+
+  @Test
+  void getHasFood_antHasFood_returnsTrue(){
+    assertEquals(cellOne.getHasFood(), true);
+  }
+
+  @Test
+  void getHomePheromone_hasPheromoneValue_returnsPheromoneValue(){
+    assertEquals(cellOne.getHomePheromone(), 10.0);
+  }
+
+  @Test
+  void getFoodPheromone_hasPheromoneValue_returnsPheromoneValue(){
+    assertEquals(cellOne.getFoodPheromone(), 25.0);
+  }
+
+  @Test
+  void getHealth_hasHealthValue_returnsHealthValue(){
+    assertEquals(cellOne.getHealth(), 500);
+  }
+
+  @Test
+  void reduceHealth_reduceBy50_returnsReducedHealthValue(){
+    cellOne.reduceHealth(50);
+    assertEquals(cellOne.getHealth(), 450);
   }
 
 }
