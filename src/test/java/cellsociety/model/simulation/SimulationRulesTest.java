63ed1d13872a4fc37d920cd40e04d2f2f5328303
Justin Aronwald <jga18@duke.edu>
2025-02-23 18:21:34 -0500

    Refactor CELL-49: Refactored all of the SimulationRules tests to handle the new neighbors

--- src/test/java/cellsociety/model/simulation/SimulationRulesTest.java
+++ src/test/java/cellsociety/model/simulation/SimulationRulesTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import cellsociety.model.Grid;
 import cellsociety.model.cell.Cell;
 import cellsociety.model.cell.DefaultCell;
+import cellsociety.model.simulation.getNeighborOptions.MooreNeighbors;
+import cellsociety.model.simulation.getNeighborOptions.VonNeumannNeighbors;
 import java.util.HashMap;
 import java.util.List;
 import java.awt.geom.Point2D.Double;
@@ -29,7 +31,7 @@ class SimulationRulesTest {
       }
     }
 
-    testSimulationRules = new SimulationRules(new HashMap<>()) {
+    testSimulationRules = new SimulationRules(new HashMap<>(), new VonNeumannNeighbors(1)) {
       @Override
       public int getNumberStates() {
         return 0;
@@ -38,36 +40,150 @@ class SimulationRulesTest {
   }
 
   @Test
-  void getNeighbors() {
+  void getNeighbors_VonNeumann_FourNeighbors() throws InvalidParameterException {
     Cell cell = grid.getCell(2, 2);
+    testSimulationRules = new SimulationRules(new HashMap<>(), new VonNeumannNeighbors(1)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
 
-    List<Cell> eightNeighbors = testSimulationRules.getNeighbors(cell, grid, true);
-    assertEquals(8, eightNeighbors.size(), "Moore's method should return 8 neighbors.");
-
-    List<Cell> fourNeighbors = testSimulationRules.getNeighbors(cell, grid, false);
-    assertEquals(4, fourNeighbors.size(), "Von neumann's method should return 4 neighbors.");
+    List<Cell> fourNeighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(4, fourNeighbors.size());
   }
 
   @Test
-  void getNeighborsBounds() {
+  void getNeighbors_Moore_EightNeighbors() throws InvalidParameterException {
+    Cell cell = grid.getCell(2, 2);
+    testSimulationRules = new SimulationRules(new HashMap<>(), new MooreNeighbors(1)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    List<Cell> eightNeighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(8, eightNeighbors.size());
+  }
+
+  @Test
+  void getNeighbors_VonNeumann_TwoLayers() throws InvalidParameterException {
+    Cell cell = grid.getCell(2, 2);
+
+    testSimulationRules = new SimulationRules(new HashMap<>(), new VonNeumannNeighbors(2)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(8, neighbors.size());
+  }
+
+  @Test
+  void getNeighbors_Moore_TwoLayers() throws InvalidParameterException {
+    Cell cell = grid.getCell(2, 2);
+
+    testSimulationRules = new SimulationRules(new HashMap<>(), new MooreNeighbors(2)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(16, neighbors.size());
+  }
+
+
+
+  @Test
+  void getNeighbors_MooreOneLayer_BoundsDifferentAreas() throws InvalidParameterException {
     Cell cell = grid.getCell(0, 0);
 
-    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid, true);
+    testSimulationRules = new SimulationRules(new HashMap<>(), new MooreNeighbors(1)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
     assertEquals(3, neighbors.size());
 
     cell = grid.getCell(0, 1);
-    neighbors = testSimulationRules.getNeighbors(cell, grid, false);
-    assertEquals(3, neighbors.size());
+    neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(5, neighbors.size());
 
-    cell = grid.getCell(0, 2);
-    neighbors = testSimulationRules.getNeighbors(cell, grid, true);
+    cell = grid.getCell(1, 0);
+    neighbors = testSimulationRules.getNeighbors(cell, grid);
     assertEquals(5, neighbors.size());
 
     cell = grid.getCell(4, 4);
-    neighbors = testSimulationRules.getNeighbors(cell, grid, false);
+    neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(3, neighbors.size());
+  }
+
+
+  @Test
+  void getNeighbors_VonNeumannOneLayer_BoundsDifferentAreas() throws InvalidParameterException {
+    Cell cell = grid.getCell(0, 0);
+
+    testSimulationRules = new SimulationRules(new HashMap<>(), new VonNeumannNeighbors(1)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
     assertEquals(2, neighbors.size());
 
+    cell = grid.getCell(0, 1);
+    neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(3, neighbors.size());
+
+    cell = grid.getCell(1, 0);
+    neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(3, neighbors.size());
+
+    cell = grid.getCell(4, 4);
+    neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(2, neighbors.size());
   }
+
+  @Test
+  void getNeighbors_MultiLayerAtCornerMoore_CorrectNeighbors() throws InvalidParameterException {
+    testSimulationRules = new SimulationRules(new HashMap<>(), new MooreNeighbors(3)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    Cell cell = grid.getCell(0, 0);
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(9, neighbors.size());
+  }
+
+  @Test
+  void getNeighbors_MultiLayerAtCornerVonNeumann_CorrectNeighbors() throws InvalidParameterException {
+    testSimulationRules = new SimulationRules(new HashMap<>(), new VonNeumannNeighbors(3)) {
+      @Override
+      public int getNumberStates() {
+        return 0;
+      }
+    };
+
+    Cell cell = grid.getCell(0, 0);
+    List<Cell> neighbors = testSimulationRules.getNeighbors(cell, grid);
+    assertEquals(6, neighbors.size());
+  }
+
+
+
 
   @Test
   void getNextState() {
