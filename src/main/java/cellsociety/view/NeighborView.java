9b9d308910f51e22a67030e22c274a9594494c94
Justin Aronwald <jga18@duke.edu>
2025-02-24 00:37:11 -0500

    Feature CELL-51B: you can now dynamically edit the neighbor information mid sim

--- src/main/java/cellsociety/view/NeighborView.java
+++ src/main/java/cellsociety/view/NeighborView.java
@@ -1,14 +1,20 @@
 package cellsociety.view;
 
+import static cellsociety.config.MainConfig.VERBOSE_ERROR_MESSAGES;
 import static cellsociety.config.MainConfig.getMessage;
 import static cellsociety.view.SidebarView.ELEMENT_SPACING;
 import static cellsociety.view.config.NeighborConfig.getAvailableNeighborTypes;
 
+import cellsociety.config.SimulationConfig;
 import cellsociety.controller.MainController;
 import cellsociety.model.simulation.InvalidParameterException;
+import cellsociety.model.simulation.Parameter;
 import cellsociety.model.simulation.Simulation;
+import cellsociety.model.simulation.SimulationMetaData;
 import cellsociety.view.components.AlertField;
 import cellsociety.view.components.IntegerField;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -16,6 +22,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class NeighborView extends VBox {
+  private final MainController mainController;
   private boolean isEditing;
   private final AlertField myAlertField;
   private IntegerField neighborLayerField;
@@ -25,6 +32,7 @@ public class NeighborView extends VBox {
 
   public NeighborView(MainController mainController, boolean isEditing) {
     this.setSpacing(ELEMENT_SPACING);
+    this.mainController = mainController;
     this.isEditing = isEditing;
     this.myAlertField = new AlertField();
 
@@ -56,9 +64,57 @@ public class NeighborView extends VBox {
   }
 
   private void updateNeighborInformation() {
+    if (neighborType.equals(neighborTypeSelector.getValue()) &&
+        neighborLayer == Integer.parseInt(neighborLayerField.getText())) {
+      return;
+    }
+
     this.neighborType = neighborTypeSelector.getValue();
-    this.neighborLayer = Integer.parseInt(neighborLayerField.getText());
+    try {
+      this.neighborLayer = Integer.parseInt(neighborLayerField.getText());
+    } catch (NumberFormatException e) {
+      myAlertField.flash(getMessage("INVALID_NEIGHBOR_LAYER"), true);
+      return;
+    }
+
+
+    Simulation currentSimulation = mainController.getSimulation();
+    Simulation newSimulation = getNewSimulation(currentSimulation);
+
+    if (newSimulation == null) {
+      return;
+    }
+    mainController.updateSimulation(newSimulation);
   }
+
+
+  private Simulation getNewSimulation(Simulation currentSimulation) {
+    Simulation newSimulation;
+    SimulationMetaData newMetaData = new SimulationMetaData(
+        currentSimulation.data().type(),
+        currentSimulation.data().name(),
+        currentSimulation.data().author(),
+        currentSimulation.data().description(),
+        neighborType,
+        neighborLayer);
+
+    try {
+      newSimulation = SimulationConfig.getNewSimulation(currentSimulation.data().type(),
+          newMetaData, currentSimulation.rules().getParameters());
+    } catch (InvocationTargetException e) {
+      myAlertField.flash(e.getCause().getMessage(), true);
+      return null;
+    } catch (ClassNotFoundException | NoSuchMethodException |
+             InstantiationException | IllegalAccessException | InvalidParameterException e) {
+      if (VERBOSE_ERROR_MESSAGES) {
+        myAlertField.flash(e.getMessage(), true);
+      }
+      throw new RuntimeException(e);
+    }
+    return newSimulation;
+  }
+
+
 
 
   private void createNeighborDisplay() {
